4a72a30616eb44a3e7d46d46403ab278b5ff0dab
Carlos Alfredo Castillo Rodriguez <cacr1990@gmail.com>
2024-07-09 19:45:50 -0400

    [ADD]: configuracion que permitira ejecutar el archivo main.ts localmente y tambien utilizando serverless

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,60 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Handler } from 'aws-lambda';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import { createServer, proxy } from 'aws-serverless-express';
+
+let cachedServer;
+
+async function bootstrapServer() {
+  const expressApp = express();
+  const adapter = new ExpressAdapter(expressApp);
+  const app = await NestFactory.create(AppModule, adapter);
+  app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle('Students API')
+    .setDescription('API for managing students')
+    .setVersion('1.0')
+    .addApiKey({ type: 'apiKey', name: 'x-api-key', in: 'header' }, 'apiKey')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.init();
+  return createServer(expressApp);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
+  app.enableCors();
+
+  const options = new DocumentBuilder()
     .setTitle('Students API')
-    .setDescription('CRUD for students using NestJS, AWS and DynamoDB')
+    .setDescription('API for managing students')
     .setVersion('1.0')
-    .addTag('students')
+    .addApiKey({ type: 'apiKey', name: 'x-api-key', in: 'header' }, 'apiKey')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
-bootstrap();
+
+let serverHandler: Handler;
+
+if (process.env.IS_OFFLINE) {
+  bootstrap();
+} else {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  serverHandler = async (event, context, callback) => {
+    if (!cachedServer) {
+      cachedServer = await bootstrapServer();
+    }
+    return proxy(cachedServer, event, context, 'PROMISE').promise;
+  };
+}
+
+export const handler = serverHandler;
