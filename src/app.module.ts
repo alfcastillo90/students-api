6dd1d8ce44314c39be909132e610300061fb61a2
Carlos Alfredo Castillo Rodriguez <cacr1990@gmail.com>
2024-07-09 18:29:05 -0400

    [ADD]: configuracion basica de variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentsModule } from './students/students.module';
 
 @Module({
-  imports: [StudentsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Hace que el ConfigModule sea globalmente accesible en toda la aplicación
+    }),
+    StudentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
