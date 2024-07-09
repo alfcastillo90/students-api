0cf14d93827a95e71e5a2f8e84b37897e2a1d268
Carlos Alfredo Castillo Rodriguez <cacr1990@gmail.com>
2024-07-09 17:59:11 -0400

    [ADD]: configuracion de swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Students API')
+    .setDescription('CRUD for students using NestJS, AWS and DynamoDB')
+    .setVersion('1.0')
+    .addTag('students')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
